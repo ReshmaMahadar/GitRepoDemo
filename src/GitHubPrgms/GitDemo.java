1de89b90676ea877fc86cdc6be6f537960945bbb
USER <USER@HP>
2020-07-01 17:25:56 +0530

    2nd time commit

--- src/GitHubPrgms/GitDemo.java
+++ src/GitHubPrgms/GitDemo.java
@@ -4,8 +4,8 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Inside GitDemo class");
-		System.out.println("Inside GitDemo.java class");
+		//System.out.println("Inside GitDemo class");
+		System.out.println("Inside GitDemo.java class");//this line got aded frm github on pulling code changes done in git
 		
 
 	}
