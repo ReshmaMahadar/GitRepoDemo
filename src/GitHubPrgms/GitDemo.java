df46d817fd69a84a9a0af5090f890b095b042ffa
USER <USER@HP>
2020-07-01 16:58:32 +0530

    SimpleDemo class

---
+++ src/GitHubPrgms/GitDemo.java
@@ -0,0 +1,11 @@
+package GitHubPrgms;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside GitDemo class");
+
+	}
+
+}
