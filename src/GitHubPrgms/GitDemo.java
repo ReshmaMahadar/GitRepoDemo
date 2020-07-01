4fe5504a70cf3b2bc675adc2e92398284b4cb997
Reshma Mahadar <37284067+ReshmaMahadar@users.noreply.github.com>
2020-07-01 17:22:13 +0530

    Update GitDemo.java

--- src/GitHubPrgms/GitDemo.java
+++ src/GitHubPrgms/GitDemo.java
@@ -5,6 +5,8 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside GitDemo class");
+		System.out.println("Inside GitDemo.java class");
+		
 
 	}
 
